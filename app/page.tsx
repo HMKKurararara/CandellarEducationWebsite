cede60eb6b6f2c4e30b6c365d10f960ff21365b0
Shade <198771302+HMKKurararara@users.noreply.github.com>
2026-07-14 16:05:23 +0800

    Combine content hub and add programme galleries

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import { content } from '@/lib/content'
 import LandingTabs from './components/LandingTabs'
 
 export default async function HomePage() {
-  const [settings, programmes, resources, posts, videos, work, team, facebook, featured] = await Promise.all([content.settings(), content.programmes(), content.resources(), content.posts(), content.videos(), content.work(), content.team(), content.facebook(), content.featured()])
-  return <LandingTabs settings={settings} programmes={programmes} resources={resources} posts={posts} videos={videos} work={work} team={team} facebook={facebook} featured={featured}/>
+  const [settings, programmes, resources, posts, videos, work, team, facebook, featured, testimonials] = await Promise.all([content.settings(), content.programmes(), content.resources(), content.posts(), content.videos(), content.work(), content.team(), content.facebook(), content.featured(), content.testimonials()])
+  return <LandingTabs settings={settings} programmes={programmes} resources={resources} posts={posts} videos={videos} work={work} team={team} facebook={facebook} featured={featured} testimonials={testimonials}/>
 }
