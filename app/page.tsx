9447963e06fd99b8b4ef6df30bb5d8a4d7926b4f
Shade <198771302+HMKKurararara@users.noreply.github.com>
2026-07-14 15:53:39 +0800

    Add featured content highlights to home

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import { content } from '@/lib/content'
 import LandingTabs from './components/LandingTabs'
 
 export default async function HomePage() {
-  const [settings, programmes, resources, posts, videos, work, team, facebook] = await Promise.all([content.settings(), content.programmes(), content.resources(), content.posts(), content.videos(), content.work(), content.team(), content.facebook()])
-  return <LandingTabs settings={settings} programmes={programmes} resources={resources} posts={posts} videos={videos} work={work} team={team} facebook={facebook}/>
+  const [settings, programmes, resources, posts, videos, work, team, facebook, featured] = await Promise.all([content.settings(), content.programmes(), content.resources(), content.posts(), content.videos(), content.work(), content.team(), content.facebook(), content.featured()])
+  return <LandingTabs settings={settings} programmes={programmes} resources={resources} posts={posts} videos={videos} work={work} team={team} facebook={facebook} featured={featured}/>
 }
